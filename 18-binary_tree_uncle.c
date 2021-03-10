7a6d7a0458d7647b7fc71e4a23c075518acc0883
vic-1998 <victor.mplaza98@gmail.com>
2021-03-10 15:23:14 +0000

    Sibling.

---
+++ 18-binary_tree_uncle.c
@@ -0,0 +1,15 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_uncle -  finds the uncle of a node
+ * @node: pointer to the node to find the sibling
+ * Return: pointer to the node to find the uncle
+ */
+binary_tree_t *binary_tree_uncle(binary_tree_t *node)
+{
+	if (node == NULL || node->parent == NULL || node->parent->parent == NULL)
+		return (NULL);
+	if (node->parent == node->parent->parent->left)
+		return (node->parent->parent->right);
+	return (node->parent->parent->left);
+}
