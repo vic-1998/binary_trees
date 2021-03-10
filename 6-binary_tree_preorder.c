3e1d467307e6be7264a020eed08cf702a84e4f7f
vic-1998 <victor.mplaza98@gmail.com>
2021-03-10 13:07:04 +0000

    Fix error betty styled.

--- 6-binary_tree_preorder.c
+++ 6-binary_tree_preorder.c
@@ -1,9 +1,11 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_preorder - function that goes through a binary tree using pre-order traversal
+ * binary_tree_preorder - function that goes through a
+ * binary tree using pre-order traversal
  * @tree: pointer to the root node of the tree to traverse
- * @func: pointer to a function to call for each node. The value in the node must be 
+ * @func: pointer to a function to call for each node. The
+ * value in the node must be
  * passed as a parameter to this function.
  * Return: void
  */
