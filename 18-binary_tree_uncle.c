9ba33ca4e177a2e6d5f8e040c69dac368d0bfdbf
vic-1998 <victor.mplaza98@gmail.com>
2021-03-11 12:52:11 +0000

    Fix betty styled error.

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -3,7 +3,6 @@
 /**
  * binary_tree_uncle -  finds the uncle of a node
  * @node: pointer to the node to find the sibling
- * 
  * Return: pointer to the node to find the uncle
  */
 binary_tree_t *binary_tree_uncle(binary_tree_t *node)
